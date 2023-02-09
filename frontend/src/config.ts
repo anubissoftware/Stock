7cf359d12954490d43cc7ddf5f6cc7b3f577da11
fibiaan <fibianmejia@hotmail.com>
2023-02-09 17:21:45 -0500

    pre production

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,7 +1,7 @@
 
-export const backendURL = "http://localhost:8012/"
-export const backendMedia = "http://localhost:8012/public"
-export const quotationURL = "http://localhost:8000/quotation/pdf/"
-
-// export const backendURL = "http://stockapi.gdlplabs.com/"
-// export const backendMedia = "http://stockapi.gdlplabs.com/public"
+export const backendURL = window.location.hostname == 'localhost' ? 
+    "http://localhost:8012/" : "http://stockapi.anubisapps.com"
+export const backendMedia = window.location.hostname == 'localhost' ?  
+    "http://localhost:8012/public" : "http://stockapi.anubisapps.com/public"
+export const quotationURL = window.location.hostname == 'localhost' ?  
+    "http://localhost:8000/quotation/pdf/" : "http://stockdoc.anubisapps.com/quotation/pdf/"
