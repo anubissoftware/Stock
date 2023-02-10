ae95f02272fa40ffdb93d8e1ec481d2e344783f9
fibiaan <fibianmejia@hotmail.com>
2023-02-10 00:02:45 -0500

    https change

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,7 +1,7 @@
 
 export const backendURL = window.location.hostname == 'localhost' ? 
-    "http://localhost:8012/" : "http://stockapi.anubisapps.com"
+    "http://localhost:8012/" : "https://stockapi.anubisapps.com"
 export const backendMedia = window.location.hostname == 'localhost' ?  
-    "http://localhost:8012/public" : "http://stockapi.anubisapps.com/public"
+    "http://localhost:8012/public" : "https://stockapi.anubisapps.com/public"
 export const quotationURL = window.location.hostname == 'localhost' ?  
     "http://localhost:8000/quotation/pdf/" : "http://stockdoc.anubisapps.com/quotation/pdf/"
